3aff745afbaf90da409c1c31b82e814a96066b25
Eragon-Drive <107802098+Eragon-Drive@users.noreply.github.com>
2022-06-19 11:38:36 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,30 @@
+function aparecer(){
+    const texto = document.querySelector('.bio')
+    if(texto){
+        texto.style = 'display: block;'
+    }
+}
+function ler(){
+    const texto = document.querySelector('.ob')
+    if(texto){
+        texto.style = 'display: block;'
+    }
+}
+function expandir(){
+    const texto = document.querySelector('.cr')
+    if(texto){
+        texto.style = 'display: block;'
+    }
+}
+function aumentar(){
+    const texto = document.querySelector('.cl')
+    if(texto){
+        texto.style = 'display: block;'
+    }
+}
+function rolar(){
+    const texto = document.querySelector('.fra')
+    if(texto){
+        texto.style = 'display: block;'
+    }
+}
